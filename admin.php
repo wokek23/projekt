b914f953b3ccc2964471bc8a90192eccc01abcf0
wokek23 <w.majchrzak@zset.leszno.pl>
2026-02-08 13:00:52 +0100

    zmiana nazw

--- admin.php
+++ admin.php
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["id"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moja strona</title>
+    <title>Zarządzanie</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/user.css">
 </head>
--- user.php
+++ user.php
@@ -50,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["id"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moja strona</title>
+    <title>Mój profil</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/user.css">
 </head>
